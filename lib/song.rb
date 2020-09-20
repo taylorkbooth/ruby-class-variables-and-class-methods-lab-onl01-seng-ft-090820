46658d7e283bda259dc8722113b6bb41452898b0
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 15:25:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,21 @@
 class Song
+
   attr_accessor :name, :artist, :genre
-  
-  @@count = 0
+
+  @@song_count = 0
   @@genres = []
   @@artists = []
-  
-  def initialize(name, artists, genres)
-    @name = name
-    @artists = artists
-    @genres << @genre
-    @@artists << @artist
-    @@count +=1
-  
-end
 
- def self.count
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
+  end
+
+  def self.count
     @@song_count
   end
 
@@ -25,7 +26,7 @@ end
   def self.artists
     @@artists.uniq
   end
-  
+
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre| 
@@ -37,7 +38,17 @@ end
     end
     genre_count
   end
-  
-  
-  
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+      artist_count[artist] +=1
+      else
+      artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
 end
