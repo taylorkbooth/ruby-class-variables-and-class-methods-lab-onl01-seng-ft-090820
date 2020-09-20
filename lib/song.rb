964e687070002acb78dfbfa191391db5ead275ef
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 15:31:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
+  @@song_count = 3
   @@genres = []
   @@artists = []
 
@@ -16,7 +16,7 @@ class Song
   end
 
   def self.count
-    @@song_count = 3
+    @@song_count
   end
 
   def self.genres
