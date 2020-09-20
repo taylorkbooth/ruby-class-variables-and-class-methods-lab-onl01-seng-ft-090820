2ed6ec0a8c58a7e51c0ab2dccb3224af452902c0
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 15:30:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,12 +28,12 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
+    genre_count = {3}
     @@genres.each do |genre| 
       if genre_count[genre]
       genre_count[genre] +=1
       else
-      genre_count[genre] = 3
+      genre_count[genre] = 1
       end
     end
     genre_count
